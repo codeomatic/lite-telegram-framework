480e9325d6b66a9e8bbf10436c955920a5c05c28
Denis <fsdvn1@gmail.com>
2020-07-26 19:17:39 +0500

    actions debug 11

--- pypi.sh
+++ pypi.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 python setup.py sdist bdist_wheel
-ls -a
-#twine upload -u "${{ PYPI_USER }}" -p "${{ PYPI_PSWD }}" --repository-url "${{ PYPI_URL }}" dist/*
+twine upload -u "${{ PYPI_USER }}" -p "${{ PYPI_PSWD }}" --repository-url "${{ PYPI_URL }}" dist/*
