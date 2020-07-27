0ba09d19a1658b473c782f9a9ed9e04e0dcc7828
Denis <fsdvn1@gmail.com>
2020-07-27 11:52:13 +0500

    Support for several simple methods was added.

--- milligram/api.py
+++ milligram/api.py
@@ -3,16 +3,38 @@ import requests
 
 class Telegram:
     url_template = 'https://api.telegram.org/bot{token}/{method_name}'
+    offset = 0
 
     def __init__(self, token):
         self.token = token
 
-    def _post(self, method_name, data):
+    def _post(self, method_name, **kwargs):
         resp = requests.post(
             self.url_template.format(token=self.token, method_name=method_name),
-            json=data
+            json=kwargs
         )
         return resp.json()
 
     def get_me(self):
-        return self._post('getMe', {})
+        return self._post('getMe')
+
+    def list_updates(self):
+        return self._post('getUpdates')
+
+    def get_updates(self):
+        resp = self._post('getUpdates', offset=self.offset)
+        if not resp['ok']:
+            return resp
+
+        result = resp['result']
+        if not result:
+            return resp
+
+        self.offset = result[-1]['update_id'] + 1
+        return resp
+
+    def get_web_hook_info(self):
+        return self._post('getWebhookInfo')
+
+    def delete_web_hook(self):
+        return self._post('deleteWebhook')
